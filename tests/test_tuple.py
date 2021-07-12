686d65ff49855a23ec58cf78e547d2ca77dee80d
Nigel Nazir <nigel@eng91147.lan>
2021-07-11 22:04:07 -0500

    Tests for subtraction

--- tests/test_tuple.py
+++ tests/test_tuple.py
@@ -25,3 +25,30 @@ def test_add_two_tuples():
     a1 = tuple.Tuple(3, -2, 5, 1)
     a2 = tuple.Tuple(-2, 3, 1, 0)
     assert a1 + a2 == tuple.Tuple(1, 1, 6, 1)
+
+
+def test_subtract_two_points():
+    '''
+    Scenario: Subtracting two points
+    '''
+    p1 = tuple.point(3, 2, 1)
+    p2 = tuple.point(5, 6, 7)
+    assert p1 - p2 == tuple.vector(-2, -4, -6)
+
+
+def test_subtract_vector_from_point():
+    '''
+    Scenario: Subtracting a vector from a point
+    '''
+    p = tuple.point(3, 2, 1)
+    v = tuple.vector(5, 6, 7)
+    assert p - v == tuple.point(-2, -4, -6)
+
+
+def test_subtract_two_vectors():
+    '''
+    Scenario: Subtracting two vectors
+    '''
+    v1 = tuple.vector(3, 2, 1)
+    v2 = tuple.vector(5, 6, 7)
+    assert v1 - v2 == tuple.vector(-2, -4, -6)
