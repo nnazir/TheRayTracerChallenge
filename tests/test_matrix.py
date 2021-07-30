c30ff1a735570c290cfcb99f002be8a2a4c5cea9
Nigel Nazir <nigelnazir@yahoo.com>
2021-07-29 22:12:54 -0500

    Added matrix multiplication

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,4 +1,4 @@
-from trtc import Matrix
+from trtc import Matrix, Tuple
 
 
 def test_construct_4x4_matrix():
@@ -74,3 +74,52 @@ def test_matrix_equality_identical_matrices():
                 [8, 7, 6, 5],
                 [4, 3, 2, 1]])
     assert A != B
+
+
+def test_multiply_two_matricies():
+    """
+    Scenario: Multiplying two matrices
+    """
+    A = Matrix([[1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 8, 7, 6],
+                [5, 4, 3, 2]])
+    B = Matrix([[-2, 1, 2, 3],
+                [3, 2, 1, -1],
+                [4, 3, 6, 5],
+                [1, 2, 7, 8]])
+    assert A * B == Matrix([[20, 22, 50, 48],
+                            [44, 54, 114, 108],
+                            [40, 58, 110, 102],
+                            [16, 26, 46, 42]])
+
+
+def test_matrix_multiplied_by_tuple():
+    """
+    Scenario: A matrix multiplied by a tuple
+    """
+    A = Matrix([[1, 2, 3, 4],
+                [2, 4, 4, 2],
+                [8, 6, 4, 1],
+                [0, 0, 0, 1]])
+    b = Tuple(1, 2, 3, 1)
+    assert A * b == Tuple(18, 24, 33, 1)
+
+
+def test_multiply_matrix_by_identity():
+    """
+    Scenario: Multiplying a matrix by the identity matrix
+    """
+    A = Matrix([[0, 1, 2, 4],
+                [1, 2, 4, 8],
+                [2, 4, 8, 16],
+                [4, 8, 16, 32]])
+    assert A * Matrix.identity_matrix() == A
+
+
+def test_multiply_identity_matrix_by_tuple():
+    """
+    Scenario: Multiplying the identity matrix by a tuple
+    """
+    a = Tuple(1, 2, 3, 4)
+    assert Matrix.identity_matrix() * a == a
--- trtc/matrix.py
+++ trtc/matrix.py
@@ -1,3 +1,5 @@
+from trtc.tuple import Tuple
+import numpy as np
 from trtc.utils import float_equal
 
 
@@ -11,3 +13,21 @@ class Matrix():
                 if not float_equal(self.matrix[row][col], o.matrix[row][col]):
                     return False
         return True
+
+    def __mul__(self, o):
+        if type(o) is Matrix:
+            return Matrix(np.matmul(self.list(), o.list()))
+        else:
+            return Tuple(np.dot(self.list()[0], o.list()),
+                         np.dot(self.list()[1], o.list()),
+                         np.dot(self.list()[2], o.list()),
+                         np.dot(self.list()[3], o.list()))
+
+    def list(self):
+        return self.matrix
+
+    def identity_matrix():
+        return Matrix([[1, 0, 0, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0],
+                       [0, 0, 0, 1]])
