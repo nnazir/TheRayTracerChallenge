aaa3aed166b361a811454a7d0a553745baebcbe6
Nigel Nazir <nigelnazir@yahoo.com>
2021-08-31 14:00:04 -0500

    Added position function to Ray and test

--- tests/test_ray.py
+++ tests/test_ray.py
@@ -1,6 +1,7 @@
 from trtc import point, vector
 from trtc import Ray
 
+
 def test_create_query_ray():
     '''
     Scenario: Creating and querying a ray
@@ -10,3 +11,16 @@ def test_create_query_ray():
     r = Ray(origin, direction)
     assert r.origin == origin
     assert r.direction == direction
+
+
+def test_compute_point_from_distance():
+    '''
+    Scenario: Computing a point from a distance
+    '''
+    r = Ray(point(2, 3, 4), vector(1, 0, 0))
+    assert r.position(0) == point(2, 3, 4)
+    assert r.position(1) == point(3, 3, 4)
+    assert r.position(-1) == point(1, 3, 4)
+    assert r.position(2.5) == point(4.5, 3, 4)
+
+
