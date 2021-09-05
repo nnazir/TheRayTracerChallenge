78d263110ba93645993038afd0c7fb1d66dcdf24
Nigel Nazir <nigelnazir@yahoo.com>
2021-09-04 21:13:26 -0500

    Moved sphere tests into test_spheres.py

---
+++ tests/test_spheres.py
@@ -0,0 +1,59 @@
+from trtc import Ray, Sphere, point, vector
+
+
+def test_ray_intersect_sphere_at_two_points():
+    '''
+    Scenario: A ray intersects a sphere at two points
+    '''
+    r = Ray(point(0, 0, -5), vector(0, 0, 1))
+    s = Sphere()
+    xs = s.intersect(r)
+    assert len(xs) == 2
+    assert xs[0] == 4.0
+    assert xs[1] == 6.0
+
+
+def test_ray_intersect_sphere_at_tangent():
+    '''
+    Scenario: A ray intersects a sphere at a tangent
+    '''
+    r = Ray(point(0, 1, -5), vector(0, 0, 1))
+    s = Sphere()
+    xs = s.intersect(r)
+    assert len(xs) == 2
+    assert xs[0] == 5.0
+    assert xs[1] == 5.0
+
+
+def test_ray_misses_shpere():
+    '''
+    Scenario: A ray misses a sphere
+    '''
+    r = Ray(point(0, 2, -5), vector(0, 0, 1))
+    s = Sphere()
+    xs = s.intersect(r)
+    assert xs == None
+
+
+def test_ray_originates_in_sphere():
+    '''
+    Scenario: A ray originates inside a sphere
+    '''
+    r = Ray(point(0, 0, 0), vector(0, 0, 1))
+    s = Sphere()
+    xs = s.intersect(r)
+    assert len(xs) == 2
+    assert xs[0] == -1.0
+    assert xs[1] == 1.0
+
+
+def test_sphere_behind_ray():
+    '''
+    Scenario: A sphere is behind a ray
+    '''
+    r = Ray(point(0, 0, 5), vector(0, 0, 1))
+    s = Sphere()
+    xs = s.intersect(r)
+    assert len(xs) == 2
+    assert xs[0] == -6.0
+    assert xs[1] == -4.0
