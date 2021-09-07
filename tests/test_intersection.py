d7bd7887b4ff55acd3ced2ba0b9da1f7b5b1c8ba
Nigel Nazir <nigelnazir@yahoo.com>
2021-09-06 21:22:30 -0500

    Added intersection hit function and __lt__ to Intersection in order to sort

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -23,3 +23,53 @@ def test_aggregating_intersections():
     assert xs.count == 2
     assert xs.intersections[0].t == 1
     assert xs.intersections[1].t == 2
+
+
+def test_hit_all_positive_t():
+    '''
+    Scenario: The hit, when all intersections have positive t
+    '''
+    s = Sphere()
+    i1 = Intersection(1, s)
+    i2 = Intersection(2, s)
+    xs = IntersectionList(i2, i1)
+    i = xs.hit()
+    assert i == i1
+
+
+def test_hit_some_negative_t():
+    '''
+    Scenario: The hit, when some intersections have negative t
+    '''
+    s = Sphere()
+    i1 = Intersection(-1, s)
+    i2 = Intersection(1, s)
+    xs = IntersectionList(i2, i1)
+    i = xs.hit()
+    assert i == i2
+
+
+def test_hit_all_test_hit_all_negative_t():
+    '''
+    Scenario: The hit, when all intersections have negative t
+    '''
+    s = Sphere()
+    i1 = Intersection(-2, s)
+    i2 = Intersection(-1, s)
+    xs = IntersectionList(i2, i1)
+    i = xs.hit()
+    assert i is None
+
+
+def test_hit_always_lowest_nonnegative():
+    '''
+    Scenario: The hit is always the lowest nonnegative intersection
+    '''
+    s = Sphere()
+    i1 = Intersection(5, s)
+    i2 = Intersection(7, s)
+    i3 = Intersection(-3, s)
+    i4 = Intersection(2, s)
+    xs = IntersectionList(i1, i2, i3, i4)
+    i = xs.hit()
+    assert i == i4
--- trtc/intersection.py
+++ trtc/intersection.py
@@ -5,8 +5,17 @@ class Intersection():
         self.t = t
         self.object = object
 
+    def __lt__(self, other):
+        return self.t < other.t
+
 
 class IntersectionList():
     def __init__(self, *argv) -> None:
         self.count = len(argv)
         self.intersections = [a for a in argv]
+        self.intersections.sort()
+
+    def hit(self):
+        for i in self.intersections:
+            if i.t > 0:
+                return i
