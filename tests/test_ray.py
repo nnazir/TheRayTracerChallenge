6e00e536ac937f27d179d22286143aa961ef445c
Nigel Nazir <nigelnazir@yahoo.com>
2021-09-07 21:04:07 -0500

    Added transform function to the Ray class

--- tests/test_ray.py
+++ tests/test_ray.py
@@ -1,5 +1,5 @@
 from trtc import point, vector
-from trtc import Ray, Sphere
+from trtc import Ray, Sphere, Matrix
 
 
 def test_create_query_ray():
@@ -22,3 +22,25 @@ def test_compute_point_from_distance():
     assert r.position(1) == point(3, 3, 4)
     assert r.position(-1) == point(1, 3, 4)
     assert r.position(2.5) == point(4.5, 3, 4)
+
+
+def test_translating_ray():
+    '''
+    Scenario: Translating a ray
+    '''
+    r = Ray(point(1, 2, 3), vector(0, 1, 0))
+    m = Matrix.translation(3, 4, 5)
+    r2 = r.transform(m)
+    assert r2.origin == point(4, 6, 8)
+    assert r2.direction == vector(0, 1, 0)
+
+
+def test_scaling_ray():
+    '''
+    Scenario: Scaling a ray
+    '''
+    r = Ray(point(1, 2, 3), vector(0, 1, 0))
+    m = Matrix.scaling(2, 3, 4)
+    r2 = r.transform(m)
+    assert r2.origin == point(2, 6, 12)
+    assert r2.direction == vector(0, 3, 0)
--- trtc/ray.py
+++ trtc/ray.py
@@ -5,3 +5,8 @@ class Ray():
 
     def position(self, t):
         return self.origin + self.direction * t
+
+    def transform(self, m):
+        origin = m * self.origin
+        direction = m * self.direction
+        return Ray(origin, direction)
