aaae5ce89e6023775c83077f8082a8b7aea5ae3c
Nigel Nazir <nigelnazir@yahoo.com>
2021-09-19 16:24:42 -0500

    Added equality test to PointLight

--- trtc/light.py
+++ trtc/light.py
@@ -1,6 +1,11 @@
 from trtc import point
 
+
 class PointLight():
     def __init__(self, position, intensity) -> None:
         self.position = position
         self.intensity = intensity
+
+    def __eq__(self, o: object) -> bool:
+        return self.position == o.position and \
+            self.intensity == o.intensity
