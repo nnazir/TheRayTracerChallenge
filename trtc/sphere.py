f3677bcb60e47fa86900e4211c64fcd417c594e5
Nigel Nazir <nigelnazir@yahoo.com>
2021-10-14 09:49:08 -0500

    Cleaned up Sphere class

--- trtc/sphere.py
+++ trtc/sphere.py
@@ -1,48 +1,8 @@
-import uuid
 import math
-import trtc
-from trtc import vector, point, Matrix, Tuple
+from trtc import vector, point
 from trtc.intersection import Intersection, IntersectionList
 from trtc.material import Material
 from trtc.shape import Shape
-
-
-
-# class Sphere():
-#     def __init__(self) -> None:
-#         self.id = uuid.uuid4()
-#         self.transform = Matrix.identity_matrix()
-#         self.material = Material()
-
-#     def intersect(self, ray):
-#         ray2 = ray.transform(self.transform.inverse())
-
-#         # the vector from the sphere's center, to the ray origin
-#         # remember: the sphere is center at the world origin
-#         sphere_to_ray = ray2.origin - point(0, 0, 0)
-#         a = ray2.direction.dot(ray2.direction)
-#         b = 2 * ray2.direction.dot(sphere_to_ray)
-#         c = sphere_to_ray.dot(sphere_to_ray) - 1
-#         discriminant = b*b - 4 * a * c
-
-#         if discriminant < 0:
-#             return IntersectionList()
-
-#         t1 = (-b - math.sqrt(discriminant)) / (2 * a)
-#         t2 = (-b + math.sqrt(discriminant)) / (2 * a)
-#         if t1 < t2:
-#             return IntersectionList(Intersection(t1, self),
-#                                     Intersection(t2, self))
-#         else:
-#             return IntersectionList(Intersection(t2, self),
-#                                     Intersection(t1, self))
-
-#     def normal_at(self, world_point):
-#         object_point = self.transform.inverse() * world_point
-#         object_normal = object_point - point(0, 0, 0)
-#         world_normal = self.transform.inverse().transpose() * object_normal
-#         world_normal.w = 0
-#         return Tuple.normalize(world_normal)
 
 
 class Sphere(Shape):
@@ -70,11 +30,5 @@ class Sphere(Shape):
         return IntersectionList(Intersection(t2, self),
                                 Intersection(t1, self))
 
-    # def local_normal_at(self, world_point):
-    #     object_point = self.transform.inverse() * world_point
-    #     object_normal = object_point - point(0, 0, 0)
-    #     world_normal = self.transform.inverse().transpose() * object_normal
-    #     world_normal.w = 0
-    #     return Tuple.normalize(world_normal)
     def local_normal_at(self, world_point):
         return vector(world_point.x, world_point.y, world_point.z)
