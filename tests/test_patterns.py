19afd35a734d71e18e04907843ef7baf186e4519
Nigel Nazir <nigelnazir@yahoo.com>
2021-10-21 20:22:48 -0500

    Added the stripe_at_object method to StripePattern

--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -1,6 +1,8 @@
+from trtc.matrix import Matrix
 import trtc.tuple
 from trtc.tuple import WHITE, BLACK, point
 from trtc.pattern import StripePattern
+from trtc.sphere import Sphere
 
 
 def test_create_stripe_pattern():
@@ -35,3 +37,31 @@ def test_stripe_pattern_alternate_x():
     assert pattern.stripe_at(point(-0.1, 0, 0)) == BLACK
     assert pattern.stripe_at(point(-1, 0, 0)) == BLACK
     assert pattern.stripe_at(point(-1.1, 0, 0)) == WHITE
+
+
+def test_stripes_with_object_transformation():
+    '''  Stripes with an object transformation  '''
+    obj = Sphere()
+    obj.transform = Matrix.scaling(2, 2, 2)
+    pattern = StripePattern(WHITE, BLACK)
+    c = pattern.stripe_at_object(obj, point(1.5, 0, 0))
+    assert c == WHITE
+
+
+def test_stripes_with_pattern_transformation():
+    '''  Stripes with a pattern transformation  '''
+    obj = Sphere()
+    pattern = StripePattern(WHITE, BLACK)
+    pattern.transform = Matrix.scaling(2, 2, 2)
+    c = pattern.stripe_at_object(obj, point(1.5, 0, 0))
+    assert c == WHITE
+
+
+def test_stripes_with_object_and_pattern_transformation():
+    '''  Stripes with both an object and a a pattern transformation  '''
+    obj = Sphere()
+    obj.transform = Matrix.scaling(2, 2, 2)
+    pattern = StripePattern(WHITE, BLACK)
+    pattern.transform = Matrix.translation(0.5, 0, 0)
+    c = pattern.stripe_at_object(obj, point(2.5, 0, 0))
+    assert c == WHITE
--- trtc/pattern.py
+++ trtc/pattern.py
@@ -1,14 +1,21 @@
 import math
 from trtc.tuple import color, point
-
+from trtc.matrix import Matrix
 
 class StripePattern():
     def __init__(self, stripe_a, stripe_b) -> None:
         self.a = stripe_a
         self.b = stripe_b
+        self.transform = Matrix.identity_matrix()
 
     def stripe_at(self, p: point) -> color:
         ''' Return the appropriate color for the pattern and point '''
         if math.floor(p.x) % 2 == 0:
             return self.a
         return self.b
+
+    def stripe_at_object(self, obj, world_point):
+        object_point = obj.transform.inverse() * world_point
+        pattern_point = self.transform.inverse() * object_point
+
+        return self.stripe_at(pattern_point)
